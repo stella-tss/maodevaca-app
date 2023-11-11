81e720281d5444ec614e9b6d240dde9de62e159b
Stella Teixeira dos Santos <stella.tss013@gmail.com>
2023-11-11 18:19:45 -0300

    commit test

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text> Mão de Vaca app </Text>
+      <Text> Mão de Vaca app! </Text>
       <StatusBar style="auto" />
     </View>
   );
